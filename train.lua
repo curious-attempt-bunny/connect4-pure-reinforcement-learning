51cd2968e8c13ccfecc8ba996aee21acf0d30fae
Merlyn <curious.attempt.bunny@gmail.com>
2016-06-02 06:48:26 -0700

    Attempt to get optim to perform better than straight nn - not working out! It performs worse ?!

--- train.lua
+++ train.lua
@@ -8,6 +8,9 @@ local target = raw:narrow(2, inputSize+1, 1)
 local size = raw:size(1) -- ~42k examples
 local validRatio = 0.15
 local nValid = math.floor(size*validRatio)
+local chunkSize = 1
+nValid = nValid - (nValid % chunkSize)
+
 local nTrain = size - nValid
 
 local train = {}
@@ -35,10 +38,57 @@ mlp:add(nn.Linear(60,1))
 -- consider: mlp:add(nn.L1Penalty(0.5))
 
 criterion = nn.MSECriterion()
-trainer = nn.StochasticGradient(mlp, criterion)
--- trainer.maxIteration = 100
-trainer.learningRate = 0.1
-trainer:train(train)
+
+require 'optim'
+
+local optimState = {learningRate=0.1}
+local params, gradParams = mlp:getParameters()
+
+local validInputs = torch.Tensor(nValid, inputSize)
+local validOutputs = torch.Tensor(nValid)
+
+for i=1,nValid do
+  validInputs[i] = torch.Tensor(valid[i][1])
+  validOutputs[i] = torch.Tensor(valid[i][2])
+end
+
+for epoch=1,25 do
+  local chunks = nValid / chunkSize
+  local shuffle_indexes = torch.randperm(nTrain)
+  local batchTrainInputs = torch.Tensor(chunkSize, inputSize)
+  local batchTrainOutputs = torch.Tensor(chunkSize) -- not sure why this had to be hardcoded
+
+  for batchIndex=0,chunks-1 do
+    for i=1,chunkSize do
+      batchTrainInputs[i] = torch.Tensor(train[shuffle_indexes[batchIndex*chunkSize+i]][1])
+      batchTrainOutputs[i] = torch.Tensor(train[shuffle_indexes[batchIndex*chunkSize+i]][2])
+    end
+
+    local function feval(params)
+      gradParams:zero()
+
+      local outputs = mlp:forward(batchTrainInputs)
+      local loss = criterion:forward(outputs, batchTrainOutputs)
+      local dloss_doutput = criterion:backward(outputs, batchTrainOutputs)
+      mlp:backward(batchTrainInputs, dloss_doutput)
+
+      -- print(loss)
+
+      return loss,gradParams
+    end
+
+    optim.sgd(feval, params, optimState)
+  end
+
+  local outputs = mlp:forward(validInputs)
+  local loss = criterion:forward(outputs, validOutputs)
+
+  print(loss)
+end
+-- trainer = nn.StochasticGradient(mlp, criterion)
+-- -- trainer.maxIteration = 100
+-- trainer.learningRate = 0.1
+-- trainer:train(train)
 -- trainer:train(valid) -- lazy eye-ball validation for a different in training error
 
 -- for i=1,10 do
